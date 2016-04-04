3dc8103b2d4f9caa5470019786b340160f37587d
Simon Keith <simonkth@gmail.com>
2016-04-04 19:10:20 +0200

    specifying CRAN repository for Rserve installation

--- Rserve/Rserve_run.R
+++ Rserve/Rserve_run.R
@@ -1,5 +1,5 @@
 # Check if Rserve is installed:
-if (!require("Rserve")) install.packages("Rserve")
+if (!require("Rserve")) install.packages("Rserve", repos="https://cloud.r-project.org/")
 
 # Start Rserve, using the conf file located in the same directory.
 # This file is simply sourcing Rserve_preload.R when starting Rserve.
